7bfb02e57af5b378e1e1c8f790ac0f88e4560af7
nutchanon-c <nutchanon.char@gmail.com>
2021-11-11 23:46:38 +0700

    layout

--- hotel_info.php
+++ hotel_info.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,42 +12,103 @@
         body {
             font-family: 'Ramabhadra', sans-serif;
         }
-</style>
+
+        .hotel_info_container {
+            /* text-align: center; */
+            width: 100vw;
+            /* height: 100vh; */
+            display: flex;
+            /* align-items: center; */
+            /* justify-content: center; */
+            vertical-align: middle;
+            background-color: #fbfbfb;
+            margin-top: 5em;
+            border-radius: 15px;
+            padding: 2em;
+            /* margin-left: 1em;
+        margin-right: 1em; */
+            border-radius: 15px;
+        }
+
+        .hotel_info_content {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            /* justify-content: center;
+            text-align: center; */
+        }
+        .hotel_info_content li{
+            padding: 0.5em;
+
+        }
+        .hotel_info_content p{
+            padding: 0.5em;
+        }
+
+        .hotel_info_content button {
+            background-color: #FD7767;
+            border: none;
+            color: white;
+            padding: 15px 30px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+
+        #book_button{
+            margin-top: 1em;
+        }
+        hr{
+            margin: 2em;
+        }
+
+        .hotel_img{
+            width: 100%;
+            height: auto;
+            border-radius: 15px;
+            border: 1px solid #fbfbfb;
+        }
+        
+
+    </style>
 </head>
+
 <body>
     <?php session_start(); ?>
-<div class="container">
+    <div class="container">
         <div class="headbar">
             <?php
-                if(isset($_GET['signin'])){
-                    echo '<a href="home.php?signin=69"><img src="./assets/logo.png" width="150px" height="150px" style="cursor: pointer;"></a>';
-                }
-                else{
-                    echo '<a href="home.php"><img src="./assets/logo.png" width="150px" height="150px" style="cursor: pointer;"></a>';
+            if (isset($_GET['signin'])) {
+                echo '<a href="home.php?signin=69"><img src="./assets/logo.png" width="150px" height="150px" style="cursor: pointer;"></a>';
+            } else {
+                echo '<a href="home.php"><img src="./assets/logo.png" width="150px" height="150px" style="cursor: pointer;"></a>';
+            }
+            ?>
+            <span>
+                <?php
+                if (isset($_GET['signin'])) {
+                    echo '<button type="submit" id="signin_button" onClick=document.location.href="profile.php?signin=69&user_id=' . $_SESSION['User_ID'] . '">' . $_SESSION['FirstName'] . '</button>';
+                    // make sign out text
+                    echo '<button type="submit" id="signout_button" onClick=document.location.href="logout.php?signin=69&user_id=' . $_SESSION['User_ID'] . '">Sign Out</button>';
+                } else {
+                    echo '<button type="submit" id="signin_button" onClick=document.location.href="login.php">Sign in</button>';
                 }
                 ?>
-                <span>
-                    <?php
-                    if(isset($_GET['signin'])){
-                        echo '<button type="submit" id="signin_button" onClick=document.location.href="profile.php?signin=69&user_id='.$_SESSION['User_ID'].'">'.$_SESSION['FirstName'].'</button>';
-                        // make sign out text
-                        echo '<button type="submit" id="signout_button" onClick=document.location.href="logout.php?signin=69&user_id='.$_SESSION['User_ID'].'">Sign Out</button>';
-                    }
-                    else{
-                        echo '<button type="submit" id="signin_button" onClick=document.location.href="login.php">Sign in</button>';
-                    }
-                    ?>
-                </span>      
-        </div> 
-    
-       
+            </span>
+        </div>
+
+
     </div>
     <div class="background_image" style="position: absolute; width: 100%">
 
-<img src="./assets/bg.png">
+        <img src="./assets/bg.png">
 
-</div>
-        <!-- 
+    </div>
+    <!-- 
             Single : room type
             xx Persons : size
             xx xx beds: bedamt, (bedtype_id -> bedtype)  ===== ex. "1 single bed"
@@ -56,40 +118,42 @@
 
 
          -->
-
-         <?php 
+    <div class="hotel_info_container">
+        <div class="hotel_info_content">
+            <?php
             //Connect to the database via connect.php
             require_once('connect.php');
             //Start the session
             // session_start();
             //Pass the value of the hotelID
             $hotelID = $_GET['hotel_id'];
-            $_SESSION['Hotel_ID']= $hotelID;
+            $_SESSION['Hotel_ID'] = $hotelID;
 
             //Query the HotelName and FullAddress from the database
-            $sql = "SELECT HotelName, FullAddress FROM Hotel WHERE Hotel_ID = '$hotelID'";
+            $sql = "SELECT HotelName, FullAddress, Picture FROM Hotel WHERE Hotel_ID = '$hotelID'";
             $result = $mysqli->query($sql);
             $row = $result->fetch_assoc();
             $hotelName = $row['HotelName'];
             $fullAddress = $row['FullAddress'];
-
+            $picture = $row['Picture'];
             // put in div
             echo '<div class="hotel_info">';
-            echo '<h1>'.$hotelName.'</h1>';
-            echo '<h2>'.$fullAddress.'</h2>';
+            echo '<img class="hotel_img" src="' . $row['Picture'] . '" alt="">';
+            echo '<h1>' . $hotelName . '</h1>';
+            echo '<h2>' . $fullAddress . '</h2>';
             echo '</div>';
 
 
 
             //Query command
             $q = "SELECT * FROM room, bedtype WHERE Hotel_ID = '$hotelID' AND room.BedType_ID = bedtype.BedType_ID";
-            
+
             // get amount of free rooms to display in dropdown
-            $d = "SELECT COUNT(*) FROM room WHERE room.Hotel_ID = '$hotelID' AND room.BedType_ID = bedtype.BedType_ID AND room.status = 0" ;
+            $d = "SELECT COUNT(*) FROM room WHERE room.Hotel_ID = '$hotelID' AND room.BedType_ID = bedtype.BedType_ID AND room.status = 0";
 
             // echo '<select>';
             // for($i = 0; $i <= 55; $i++){
-               
+
             //     echo "<option value=".$i.">".$i."</option>";
             //     // echo $i;
 
@@ -101,70 +165,70 @@
             // 2. update those status of room_id to 1 WHEN DOING TRANSACTION FROM TRANSACTION PAGE
 
             //Execute the query
-            if($result = $mysqli->query($q)){
+            if ($result = $mysqli->query($q)) {
                 //While loop for displaying information
-                while($row = $result->fetch_array()){
-                    echo '<div>'; //The most outer container of the box of information (Vertical box)
-                        echo '<h2>'.$row['Room_Type'].'</h2>';
-                        echo '<list></list>';//Start of the list contain information
-                            echo '<ul>';//Unordered list
-                                echo '<li>'.$row['Size'].' Person</li>';
-                                echo '<li>'.$row['BedAmt']." ".$row['BedType']." beds</li>";
-                            echo '</ul>';
-                        echo '</list>';
-                        echo '<p>'.$row['Room_Desc'].'</p>';
-                        echo '<h2>'.$row["Price"].' Baht/Night</h2>';
+                while ($row = $result->fetch_array()) {
+                    echo '<div class="hotel_info_list">'; //The most outer container of the box of information (Vertical box)
+                    echo '<h2>' . $row['Room_Type'] . '</h2>';
+                    echo '<list></list>'; //Start of the list contain information
+                    echo '<ul>'; //Unordered list
+                    echo '<li>' . $row['Size'] . ' Persons</li>';
+                    echo '<li>' . $row['BedAmt'] . " " . $row['BedType'] . " beds</li>";
+                    echo '</ul>';
+                    echo '</list>';
+                    echo '<p>' . $row['Room_Desc'] . '</p>';
+                    echo '<h2>' . $row["Price"] . ' Baht / Night</h2>';
                     //Drop down list for #room avaliable
-                    $d = "SELECT COUNT(*) AS num FROM room WHERE room.Hotel_ID = '$hotelID' AND room.Room_Type = '".$row['Room_Type']."' AND room.status = 0 AND room.bedType_ID = ".$row['BedType_ID'].' ;';
-                    
-                    
-                    
-                        if($inner = $mysqli->query($d)){
-                            // $temp=0;
-                            while($row2 = $inner->fetch_array() /* and $temp<100 */){
-                                // echo "<option value=".$row['num']."></option>";
-                                // $temp++;
-                                // echo $row2['num'];
-                                if (isset($_GET['signin'])){
-                                    echo '<form action="reservation_req.php?signin=69" method="GET">';
-                                    echo '<input type="hidden" name="signin" value="69"/>';
-                                }
-                                else{
-                                    echo '<form action="login.php">';
-                                }
-                                
-                                // echo "<select name = ".$row['Room_Type'].">";
-                                // for($i = 1; $i <= intval($row2['num']); $i++){
-                                //     echo "<option value=".$i.">".$i."</option>";
-                                //     echo $i;
-                                // }
-                                // echo "</select>";
-                                echo '
-                                <input type="hidden" name="RoomType" value="'.$row['Room_Type'].'"/>
-                                <input type="hidden" name="BedTypeID" value="'.$row['BedType_ID'].'"/>
-                                <input type="hidden" name="HotelID" value="'.$row['Hotel_ID'].'"/>';
-                                
-                                if (isset($_GET['signin'])){
-                                    echo '<input type="hidden" name="User_ID" value="'.$_SESSION['User_ID'].'"/>';
-                                }
+                    $d = "SELECT COUNT(*) AS num FROM room WHERE room.Hotel_ID = '$hotelID' AND room.Room_Type = '" . $row['Room_Type'] . "' AND room.status = 0 AND room.bedType_ID = " . $row['BedType_ID'] . ' ;';
 
-                                echo '<button name ="submit" value="1" type="submit">Book Now</button>';
-                                echo "</form>";
+
+
+                    if ($inner = $mysqli->query($d)) {
+                        // $temp=0;
+                        while ($row2 = $inner->fetch_array() /* and $temp<100 */) {
+                            // echo "<option value=".$row['num']."></option>";
+                            // $temp++;
+                            // echo $row2['num'];
+                            if (isset($_GET['signin'])) {
+                                echo '<form action="reservation_req.php?signin=69" method="GET">';
+                                echo '<input type="hidden" name="signin" value="69"/>';
+                            } else {
+                                echo '<form action="login.php">';
                             }
+
+                            // echo "<select name = ".$row['Room_Type'].">";
+                            // for($i = 1; $i <= intval($row2['num']); $i++){
+                            //     echo "<option value=".$i.">".$i."</option>";
+                            //     echo $i;
+                            // }
+                            // echo "</select>";
+                            echo '
+                                <input type="hidden" name="RoomType" value="' . $row['Room_Type'] . '"/>
+                                <input type="hidden" name="BedTypeID" value="' . $row['BedType_ID'] . '"/>
+                                <input type="hidden" name="HotelID" value="' . $row['Hotel_ID'] . '"/>';
+
+                            if (isset($_GET['signin'])) {
+                                echo '<input type="hidden" name="User_ID" value="' . $_SESSION['User_ID'] . '"/>';
+                            }
+                            echo '<button id="book_button" name ="submit" value="1" type="submit">Book Now</button>';
+                            echo "</form>";
+                            echo '<hr>';
                         }
-                        else{
-                                echo "query failed:".$mysqli->error;
-                            }
-                    
+                    } else {
+                        echo "query failed:" . $mysqli->error;
+                    }
 
-                    echo '</div>';//Close tag of the most outer container
+
+                    echo '</div>'; //Close tag of the most outer container
                 }
             }
-            
-            
 
 
-         ?>
+
+
+            ?>
+        </div>
+    </div>
 
 </body>
 
